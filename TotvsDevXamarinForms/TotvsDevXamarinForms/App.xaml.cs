64bb19cd18f2fb886bdcdcf0847103432845bd07
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-07-15 09:40:10 -0300

    Initial commit

---
+++ TotvsDevXamarinForms/TotvsDevXamarinForms/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TotvsDevXamarinForms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            AppCenter.Start("android=88792496-a93c-46f0-80cb-03dda2ef1e1c;" +
+                  "ios=8aa53e88-a408-4ca7-85b4-6c5f8580f8b2",
+                  typeof(Analytics), typeof(Crashes));
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TotvsDevXamarinForms/TotvsDevXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TotvsDevXamarinForms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnalytics_Clicked(object sender, EventArgs e)
+        {
+            var properties = new Dictionary<string, string> {
+                { "Category", "Music" }
+            };
+
+            Analytics.TrackEvent("Pagina acessada",properties);
+        }
+
+        private void btnCrash_Clicked(object sender, EventArgs e)
+        {
+            Crashes.GenerateTestCrash();
+
+            //try
+            //{
+               
+            //}
+            //catch (Exception exception)
+            //{
+            //    Crashes.TrackError(exception);
+            //}
+
+            //try
+            //{
+            //    // your code here.
+            //}
+            //catch (Exception exception)
+            //{
+            //                var properties = new Dictionary<string, string> {
+            //    { "Category", "Music" },
+            //    { "Wifi", "On" }
+            //  };
+            //    Crashes.TrackError(exception, properties);
+            //}
+        }
+    }
+}
